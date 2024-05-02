30085ff3833c38ea4221548358970c67b24c73fd
ByICE-ByIDE <byiceide@hotmail.com>
2024-05-02 17:17:18 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# 拜语言第三方库 [byExternal]
+
+发布日期:2024-5-1
+
+安全RSA加密、解密，同时支持:javascript、java、c#，世界最简单的RSA应用方案，没有之一。
+
+邮件发送：支持pop3，获取计算机硬件信息等。
+
+js 库基于原生 https://github.com/travist/jsencrypt 制作的，js库，java 和 c# 则是拜语言官方实现的。
---
+++ byExternal/src/byExternal/random.js
@@ -0,0 +1,21 @@
+﻿window.byExternal_random = ﻿class random
+{
+    static next()
+    {
+        if(arguments.length == 2)
+        {
+            return Math.floor(Math.random()*(arguments[1] - arguments[0] + 1) + arguments[0]);
+        }
+        else if(arguments.length == 1)
+        {
+            let min = 1;
+            return Math.floor(Math.random()*(arguments[0] - min + 1) + min);
+        }
+        else if(arguments.length == 0)
+        {
+            let max = 100;
+            let min = 1;
+            return Math.floor(Math.random()*(max - min + 1) + min);
+        }
+    }
+};
